f110adbc196d476923c330e10f96ea64e044298a
Leonard Pouliot <leonard.pouliot@polymtl.com>
2023-10-31 14:46:07 -0400

    Fist commit

---
+++ GuerreDesChiffres.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+//...
+
+// fonction exécutée par les producteurs
+void* producteur( void* pid) {
+ 
+ // ...
+   return NULL;             
+}
+
+// fonction exécutée par les consommateurs
+void* consommateur(void *cid) {
+   // ...
+    
+  return NULL;   
+  
+}
+
+// ...
+
+// fonction main
+int main(int argc, char* argv[]) {
+   /* Les paramètres du programme sont, dans l’ordre :
+      le nombre de producteurs, le nombre de consommateurs
+      et la taille du tampon.*/
+    
+  // ..
+    return 0;
+}
